e6d1054969a4714a42f7f26dda2ef682ebc5b60d
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2023-12-01 10:16:17 +0000

    Day 01, part 2
    Really got tripped up by the test cases not covering 'eighthree' -> 83
    
    Managed to spot it without looking it up, but I'm clearly not the only one, see
    https://www.reddit.com/r/adventofcode/comments/1884fpl/2023_day_1for_those_who_stuck_on_part_2/

--- advent_of_code/day_01/day_01.py
+++ advent_of_code/day_01/day_01.py
@@ -1,10 +1,56 @@
 from typing import List
+from dataclasses import dataclass
 from string import ascii_lowercase
+
+REPLACE_DIGITS = {
+    "one": "o1e",
+    "two": "t2o",
+    "three": "t3e",
+    "four": "f4r",
+    "five": "f5e",
+    "six": "s6x",
+    "seven": "s7n",
+    "eight": "e8t",
+    "nine": "n9e",
+}
 
 REMOVE_LETTERS = {ord(i): None for i in ascii_lowercase}
 
 type Line = str
+type Digit = str
 type CalibrationValue = int
+
+
+@dataclass
+class SearchResult:
+    found: bool
+    digit: Digit
+
+
+def search_for_first_spelled_digit(line: Line) -> SearchResult:
+    locations = {}
+    for digit in REPLACE_DIGITS.keys():
+        location = line.find(digit)
+        if location != -1:
+            locations[digit] = location
+
+    if len(locations) == 0:
+        return SearchResult(False, "")
+
+    return SearchResult(True, min(locations.items(), key=lambda l: l[1])[0])
+
+
+def replace_spelled_with_digits(line: Line) -> Line:
+    found = True
+    new_line = line
+    while found:
+        result = search_for_first_spelled_digit(new_line)
+        if result.found:
+            new_line = new_line.replace(result.digit, REPLACE_DIGITS[result.digit])
+
+        found = result.found
+
+    return new_line
 
 
 def remove_letters(line: Line) -> Line:
@@ -23,4 +69,12 @@ if __name__ == "__main__":
     with open("./advent_of_code/day_01/day_01.txt", "r") as file:
         calibration_lines = [line.strip() for line in file.readlines()]
 
+    print("part 1")
     print(sum_calibration_values(calibration_lines))
+
+    print("part 2")
+    print(
+        sum_calibration_values(
+            [replace_spelled_with_digits(l) for l in calibration_lines]
+        )
+    )
--- advent_of_code/day_01/test_day_01.py
+++ advent_of_code/day_01/test_day_01.py
@@ -1,4 +1,11 @@
-from day_01 import remove_letters, get_first_last, sum_calibration_values
+from day_01 import (
+    remove_letters,
+    get_first_last,
+    replace_spelled_with_digits,
+    sum_calibration_values,
+    search_for_first_spelled_digit,
+    SearchResult,
+)
 
 FIXTURES = [
     ("1abc2", "12", 12),
@@ -6,6 +13,28 @@ FIXTURES = [
     ("a1b2c3d4e5f", "12345", 15),
     ("treb7uchet", "7", 77),
 ]
+
+PART_2_FIXTURES = [
+    ("two1nine", "t2o1n9e", 29),
+    ("eightwothree", "e8t2ot3e", 83),
+    ("abcone2threexyz", "abco1e2t3exyz", 13),
+    ("xtwone3four", "xt2o1e3f4r", 24),
+    ("4nineeightseven2", "4n9ee8ts7n2", 42),
+    ("zoneight234", "zo1e8t234", 14),
+    ("7pqrstsixteen", "7pqrsts6xteen", 76),
+    ("eighthree", "e8t3e", 83),
+    ("sevenine", "s7n9e", 79),
+]
+
+
+def test_search_for_first_spelled_digit():
+    assert search_for_first_spelled_digit("eightwothree") == SearchResult(True, "eight")
+    assert search_for_first_spelled_digit("8wo3") == SearchResult(False, "")
+
+
+def test_replace_spelled_with_digits():
+    for input, output, _ in PART_2_FIXTURES:
+        assert output == replace_spelled_with_digits(input)
 
 
 def test_remove_letters():
@@ -19,5 +48,12 @@ def test_get_first_last():
 
 
 def test_sum_calibration_values():
+    # part 1
     FIXTURE_SUM = sum(i for _, _, i in FIXTURES)
     assert FIXTURE_SUM == sum_calibration_values([l for l, _, _ in FIXTURES])
+
+    # part 2
+    PART_2_FIXTURE_SUM = sum(i for _, _, i in PART_2_FIXTURES)
+    assert PART_2_FIXTURE_SUM == sum_calibration_values(
+        [replace_spelled_with_digits(l) for l, _, _ in PART_2_FIXTURES]
+    )
