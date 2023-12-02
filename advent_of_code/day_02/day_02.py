e65d0eaae85e5bdda9303dbacbaf72fc6b9601ad
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2023-12-02 07:57:46 +0000

    Day 02, part 1

---
+++ advent_of_code/day_02/day_02.py
@@ -0,0 +1,93 @@
+from dataclasses import dataclass
+from collections import namedtuple
+from typing import List
+import re
+
+MAX_RED = 12
+MAX_BLUE = 14
+MAX_GREEN = 13
+
+MATCH_GAME_ID = re.compile(r"Game (\d+): ")
+MATCH_RED = re.compile(r"(\d+) red")
+MATCH_BLUE = re.compile(r"(\d+) blue")
+MATCH_GREEN = re.compile(r"(\d+) green")
+
+type Line = str
+
+Handful = namedtuple("Handful", "red, blue, green")
+
+
+@dataclass
+class Game:
+    id: int
+    handfuls: List[Handful]
+
+    def is_possible(self) -> bool:
+        return all(is_possible_handful(h) for h in self.handfuls)
+
+    def minimum_handful(self) -> Handful:
+        if len(self.handfuls) == 0:
+            return Handful(0, 0, 0)
+
+        max_red = max(h.red for h in self.handfuls)
+        max_blue = max(h.blue for h in self.handfuls)
+        max_green = max(h.green for h in self.handfuls)
+
+        return Handful(max_red, max_blue, max_green)
+
+
+def parse_handfuls(line: Line) -> List[Handful]:
+    handfuls = []
+    parts = [p.strip() for p in line.split(";")]
+    for part in parts:
+        red = 0
+        blue = 0
+        green = 0
+        red_search = re.search(MATCH_RED, part)
+        if red_search:
+            red = int(red_search.group(1))
+
+        blue_search = re.search(MATCH_BLUE, part)
+        if blue_search:
+            blue = int(blue_search.group(1))
+
+        green_search = re.search(MATCH_GREEN, part)
+        if green_search:
+            green = int(green_search.group(1))
+
+        handfuls.append(Handful(red, blue, green))
+    return handfuls
+
+
+def is_possible_handful(handful: Handful) -> bool:
+    return (
+        (handful.red <= MAX_RED)
+        & (handful.blue <= MAX_BLUE)
+        & (handful.green <= MAX_GREEN)
+    )
+
+
+def parse_line(line: Line) -> Game:
+    id_search = re.search(MATCH_GAME_ID, line)
+
+    if id_search:
+        game_id = int(id_search.group(1))
+
+        handfuls = parse_handfuls(line.replace(id_search.group(0), ""))
+
+        return Game(game_id, handfuls)
+
+    raise ValueError("No game found" + line)
+
+
+def sum_possible_game_ids(lines: List[Line]) -> int:
+    games = [parse_line(l) for l in lines]
+    return sum(g.id for g in games if g.is_possible())
+
+
+if __name__ == "__main__":
+    with open("./advent_of_code/day_02/day_02.txt", "r") as file:
+        lines = [line.strip() for line in file.readlines()]
+
+    print("part 1")
+    print(sum_possible_game_ids(lines))
---
+++ advent_of_code/day_02/test_day_02.py
@@ -0,0 +1,64 @@
+from day_02 import (
+    parse_handfuls,
+    Handful,
+    Game,
+    is_possible_handful,
+    parse_line,
+    sum_possible_game_ids,
+)
+
+PART_1_FIXTURE = [
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+]
+
+
+def test_parse_handfuls():
+    assert parse_handfuls("3 blue, 4 red") == [Handful(4, 3, 0)]
+    assert parse_handfuls(" 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green") == [
+        Handful(4, 3, 0),
+        Handful(1, 6, 2),
+        Handful(0, 0, 2),
+    ]
+
+
+def test_is_possible_handful():
+    assert is_possible_handful(Handful(1, 2, 3))
+    assert not is_possible_handful(Handful(500, 3, 4))
+    assert not is_possible_handful(Handful(1, 500, 4))
+    assert not is_possible_handful(Handful(2, 2, 500))
+
+
+def test_parse_line():
+    game = parse_line("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+    assert game.id == 1
+    assert game.handfuls == [
+        Handful(4, 3, 0),
+        Handful(1, 6, 2),
+        Handful(0, 0, 2),
+    ]
+
+
+def test_game_possible():
+    assert Game(
+        1,
+        [
+            Handful(4, 3, 0),
+            Handful(1, 6, 2),
+            Handful(0, 0, 2),
+        ],
+    ).is_possible()
+
+    assert not Game(
+        1,
+        [
+            Handful(1000, 3, 0),
+        ],
+    ).is_possible()
+
+
+def test_sum_possible_games():
+    assert sum_possible_game_ids(PART_1_FIXTURE) == 8
